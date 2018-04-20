2cb824d2362ee016a1afe77d313efe135da0b156
cs4171 <cs4171@SKCS4171>
2018-04-20 12:00:40 +0800

    Refectoring kata

---
+++ TennisGameKata/TennisGame.cs
@@ -0,0 +1,93 @@
+﻿namespace TennisGameKata
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class TennisGame
+    {
+        private string _firstPlayerName;
+
+        private string _secondPlayerName;
+
+        public TennisGame(string firstPlayerName, string secondPlayerName)
+        {
+            this._firstPlayerName = firstPlayerName;
+            this._secondPlayerName = secondPlayerName;
+        }
+
+        private Dictionary<int, string> scoreLookup = new Dictionary<int, string>()
+                                                          {
+                                                              { 0, "Love" },
+                                                              { 1, "Fifteen" },
+                                                              { 2, "Thirty" },
+                                                              { 3, "Forty" },
+                                                          };
+
+        private int _firstPlayerScoreTimes;
+
+        private int _secondPlayerScoreTimes;
+
+        /// <summary>
+        /// The score.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        public string Score()
+        {
+            if (this._firstPlayerScoreTimes > 3 || this._secondPlayerScoreTimes > 3)
+            {
+                if (this._firstPlayerScoreTimes - this._secondPlayerScoreTimes >= 2)
+                {
+                    return this._firstPlayerName + " Win";
+                }
+
+                if (this._firstPlayerScoreTimes - this._secondPlayerScoreTimes <= -2)
+                {
+                    return this._secondPlayerName + " Win";
+                }
+            }
+
+            if (this._firstPlayerScoreTimes >= 3 && this._secondPlayerScoreTimes >= 3)
+            {
+                if (this._firstPlayerScoreTimes == this._secondPlayerScoreTimes)
+                {
+                    return "Deuce";
+                }
+
+                if (this._firstPlayerScoreTimes == this._secondPlayerScoreTimes + 1)
+                {
+                    return this._firstPlayerName + " Adv";
+                }
+
+                if (this._firstPlayerScoreTimes + 1 == this._secondPlayerScoreTimes)
+                {
+                    return this._secondPlayerName + " Adv";
+                }
+            }
+
+            if (this._firstPlayerScoreTimes == this._secondPlayerScoreTimes)
+            {
+                return this.scoreLookup[this._firstPlayerScoreTimes] + " All";
+            }
+
+            return this.scoreLookup[this._firstPlayerScoreTimes] + " " + this.scoreLookup[this._secondPlayerScoreTimes];
+        }
+
+        /// <summary>
+        /// The first player score.
+        /// </summary>
+        public void FirstPlayerScore()
+        {
+            this._firstPlayerScoreTimes++;
+        }
+
+        /// <summary>
+        /// The second player score.
+        /// </summary>
+        public void SecondPlayerScore()
+        {
+            this._secondPlayerScoreTimes++;
+        }
+    }
+}
--- TennisGameKata/UnitTest1.cs
+++ TennisGameKata/UnitTest1.cs
@@ -6,9 +6,152 @@ namespace TennisGameKata
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private readonly TennisGame tennisGame;
+
+        public UnitTest1()
         {
+            this.tennisGame = new TennisGame("Joey", "James");
+        }
+
+        [TestMethod]
+        public void Game_Start_Should_Be_Love_All()
+        {
+            Assert.AreEqual("Love All", this.tennisGame.Score());
+        }
+
+        [TestMethod]
+        public void Fifteen_Love()
+        {
+            this.GivenFirstPlayerScoreTimes(1);
+            this.ScoreShouldBe("Fifteen Love");
+        }
+
+        [TestMethod]
+        public void Thirty_Love()
+        {
+            this.GivenFirstPlayerScoreTimes(2);
+            this.ScoreShouldBe("Thirty Love");
+        }
+
+        [TestMethod]
+        public void Forty_Love()
+        {
+            this.GivenFirstPlayerScoreTimes(3);
+            this.ScoreShouldBe("Forty Love");
+        }
+
+        [TestMethod]
+        public void Love_Fifteen()
+        {
+            this.GivenSecondPlayerScoreTimes(1);
+            this.ScoreShouldBe("Love Fifteen");
+        }
+
+        [TestMethod]
+        public void Love_Thirty()
+        {
+            this.GivenSecondPlayerScoreTimes(2);
+            this.ScoreShouldBe("Love Thirty");
+        }
+
+        [TestMethod]
+        public void Love_Forty()
+        {
+            this.GivenSecondPlayerScoreTimes(3);
+            this.ScoreShouldBe("Love Forty");
+        }
+
+        [TestMethod]
+        public void Fifteen_All()
+        {
+            this.GivenFirstPlayerScoreTimes(1);
+            this.GivenSecondPlayerScoreTimes(1);
+            this.ScoreShouldBe("Fifteen All");
+        }
+
+        [TestMethod]
+        public void Thirty_All()
+        {
+            this.GivenFirstPlayerScoreTimes(2);
+            this.GivenSecondPlayerScoreTimes(2);
+            this.ScoreShouldBe("Thirty All");
+        }
+
+        [TestMethod]
+        public void Deuce_When_3_3()
+        {
+            this.GivenFirstPlayerScoreTimes(3);
+            this.GivenSecondPlayerScoreTimes(3);
+            this.ScoreShouldBe("Deuce");
+        }
+
+        [TestMethod]
+        public void Deuce_When_4_4()
+        {
+            this.GivenFirstPlayerScoreTimes(4);
+            this.GivenSecondPlayerScoreTimes(4);
+            this.ScoreShouldBe("Deuce");
+        }
+
+        [TestMethod]
+        public void Deuce_When_4_3()
+        {
+            this.GivenFirstPlayerScoreTimes(4);
+            this.GivenSecondPlayerScoreTimes(3);
+            this.ScoreShouldBe("Joey Adv");
+        }
+
+        [TestMethod]
+        public void Deuce_When_3_4()
+        {
+            this.GivenFirstPlayerScoreTimes(3);
+            this.GivenSecondPlayerScoreTimes(4);
+            this.ScoreShouldBe("James Adv");
+        }
+
+        [TestMethod]
+        public void Win_When_4_0()
+        {
+            this.GivenFirstPlayerScoreTimes(4);
+            this.GivenSecondPlayerScoreTimes(0);
+            this.ScoreShouldBe("Joey Win");
+        }
+
+        [TestMethod]
+        public void Win_When_0_4()
+        {
+            this.GivenFirstPlayerScoreTimes(0);
+            this.GivenSecondPlayerScoreTimes(4);
+            this.ScoreShouldBe("James Win");
+        }
+
+        [TestMethod]
+        public void Win_When_7_5()
+        {
+            this.GivenFirstPlayerScoreTimes(7);
+            this.GivenSecondPlayerScoreTimes(5);
+            this.ScoreShouldBe("Joey Win");
+        }
+
+        private void GivenFirstPlayerScoreTimes(int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                this.tennisGame.FirstPlayerScore();
+            }
+        }
+
+        private void GivenSecondPlayerScoreTimes(int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                this.tennisGame.SecondPlayerScore();
+            }
+        }
+
+        private void ScoreShouldBe(string expected)
+        {
+            Assert.AreEqual(expected, this.tennisGame.Score());
         }
     }
 }
